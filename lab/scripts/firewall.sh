74719010d3180077280b9e5e68c45f657409a5a3
Emerson Maciel <59801743+Emersoft76@users.noreply.github.com>
2025-04-10 13:42:14 +0100

    firewall.sh

--- lab/scripts/firewall.sh
+++ lab/scripts/firewall.sh
@@ -2,24 +2,14 @@
 
 # Script básico de firewall com iptables
 
-# Política padrão
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
-# Permitir loopback
 iptables -A INPUT -i lo -j ACCEPT
-
-# Permitir conexões já estabelecidas
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# Permitir SSH (porta 22)
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
-# Permitir OpenVPN (porta UDP 1194)
 iptables -A INPUT -p udp --dport 1194 -j ACCEPT
-
-# Permitir ping
 iptables -A INPUT -p icmp -j ACCEPT
 
 echo "Firewall aplicado com sucesso!"
